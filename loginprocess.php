0febed0c8832faaaba6c9de7550eff6d5d3e37ce
ComputingGod <gibbard.o@oundleschool.org.uk>
2021-11-30 10:21:03 +0000

    Spearow

---
+++ loginprocess.php
@@ -0,0 +1,33 @@
+<?php 
+
+include_once ("connection.php"); 
+
+array_map("htmlspecialchars", $_POST); 
+
+$stmt = $conn->prepare("SELECT * FROM tblusers WHERE surname =:username ;" ); 
+
+$stmt->bindParam(':username', $_POST['Username']); 
+
+$stmt->execute(); 
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+
+{  
+
+    if($row['Password']== $_POST['Pword']){ 
+
+        header('Location: users.php'); 
+
+    }else{ 
+
+ 
+
+        header('Location: login.php'); 
+
+    } 
+
+} 
+
+$conn=null; 
+
+?> 
